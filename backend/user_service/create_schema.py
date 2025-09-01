274279036c91921b36d8fc4ec77da13cb9d5606a
alex <root@DESKTOP-QNA7NCI.localdomain>
2025-09-01 16:38:49 +0300

    add create_schema_script

---
+++ backend/user_service/create_schema.py
@@ -0,0 +1,12 @@
+# create_schema.py
+import os
+import psycopg2
+
+db_url = os.environ["DATABASE_URL"].replace("&channel_binding=require", "")
+conn = psycopg2.connect(db_url)
+conn.autocommit = True
+cur = conn.cursor()
+cur.execute("CREATE SCHEMA IF NOT EXISTS users_schema; GRANT ALL ON SCHEMA users_schema TO dev;")
+cur.close()
+conn.close()
+print("Schema created successfully!")
