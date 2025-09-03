638803b38e24d2d17de0db0f424d931c5977e64f
alex <root@DESKTOP-QNA7NCI.localdomain>
2025-09-03 18:52:21 +0300

    disable router

--- backend/user_service/user_service/urls.py
+++ backend/user_service/user_service/urls.py
@@ -35,23 +35,24 @@ def debug_host(request):
     })
 
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='users')
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r'users-list', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('health/', HealthCheckView.as_view(), name='health'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('verify-email/<str:uidb64>/<str:token>/', VerifyEmailView.as_view(), name='verify-email'),
-    path('resend-verification/', ResendVerificationCodeView.as_view(), name='resend-verification'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
-    path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset'),
-    path('password-reset-confirm/<str:uidb64>/<str:token>/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('schema/', SpectacularAPIView.as_view(renderer_classes=[OpenApiJsonRenderer]), name='schema'),  # Додано для документації
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Додано для Swagger UI
+    path('admin', admin.site.urls),
+    path('health', HealthCheckView.as_view(), name='health'),
+    path('register', RegisterView.as_view(), name='register'),
+    path('verify-email/<str:uidb64>/<str:token>', VerifyEmailView.as_view(), name='verify-email'),
+    path('resend-verification', ResendVerificationCodeView.as_view(), name='resend-verification'),
+    path('login', LoginView.as_view(), name='login'),
+    path('token/refresh', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('password-reset', PasswordResetRequestView.as_view(), name='password-reset'),
+    path('password-reset-confirm/<str:uidb64>/<str:token>', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+    path('profile', UserProfileView.as_view(), name='profile'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('schema', SpectacularAPIView.as_view(renderer_classes=[OpenApiJsonRenderer]), name='schema'),  # Додано для документації
+    path('swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Додано для Swagger UI
 ] + router.urls + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
