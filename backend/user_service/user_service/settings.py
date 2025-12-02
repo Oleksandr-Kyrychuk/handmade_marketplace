50eb3218c1ee9facde23ccc6240dbb3f7f5fcc30
root <root@DESKTOP-S3933CE.localdomain>
2025-12-02 18:08:08 +0200

    fix: enable JWT authentication globally

--- backend/user_service/user_service/settings.py
+++ backend/user_service/user_service/settings.py
@@ -80,6 +80,12 @@ DATABASES = {
 DATABASES['default']['OPTIONS'] = {'options': '-c search_path=users_schema,public'}
 
 REST_FRAMEWORK = {
+'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
